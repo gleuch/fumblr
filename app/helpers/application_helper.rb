81442d91252e736343efda34286b77fee8951e60
Greg Leuch <gleuch@halvfet.local>
2010-01-21 16:07:55 -0800

    starting simple templating, adding in routes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+
+  def get_page_title(str='')
+    title = []
+    title << str unless str.blank?
+    title << @title unless str.blank?
+
+    "#{title.join(' - ')}#{!title.blank? ? ' | ' : ''}#{configatron.site_name}"
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.resource :user_session
-
   map.resource :account, :controller => 'users'
   map.resources :users
 
+  map.login '/login', :controller => 'user_sessions', :action => 'new'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.invite '/invite/:invite_id/:id', :controller => 'users', :action => 'activate'
 
+  map.dashboard '/dashboard', :controller => 'home', :action => 'index'
   map.root :controller => 'home', :action => 'index'
 
   map.connect ':controller/:action/:id'
